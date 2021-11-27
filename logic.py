f20e16eb3841bd59dad3c6f953dd87a1d90756db
Emil Vikström <emil.vikstrom@barium.se>
2021-11-27 04:38:30 +0100

    Choose preferred moves and avoid disliked ones

--- logic.py
+++ logic.py
@@ -10,8 +10,10 @@ def get_move(state: MoveRequest) -> MoveResponse:
     you = state.you
 
     all_moves: Set[Move] = {"up", "down", "left", "right"}
-    possible_moves: Set[Move] = all_moves.copy()
     opposites: Dict[Move, Move] = {"up": "down", "down": "up", "left": "right", "right": "left"}
+    possible_moves: Set[Move] = all_moves.copy()
+    disliked_moves: Set[Move] = set()
+    preferred_moves: Set[Move] = set()
 
     x = you.head.x
     y = you.head.y
@@ -35,19 +37,52 @@ def get_move(state: MoveRequest) -> MoveResponse:
         if next_square.body:
             possible_moves.discard(move)
 
-    # Pick move
-    my_move: Move = "up"
-    if current_direction in possible_moves:
-        print("pick current direction")
-        my_move = current_direction
-    elif possible_moves:
-        print("pick random")
-        my_move = random.choice(list(possible_moves))
-
+    # Dislike border moves
     for move in possible_moves:
         sq = head.move(move)
-        if sq.food:
-            print("pick food")
-            my_move = move
+        if sq and sq.is_border:
+            disliked_moves.add(move)
+
+    # Dislike moves with opponent head two steps away
+    # Prefer moves with food 1-2 steps away
+    for move1 in possible_moves:
+        first = head.move(move1)
+        if first:
+            if first.food and you.health <= 50:
+                preferred_moves.add(move1)
+            elif first.food and you.health >= 80:
+                disliked_moves.add(move1)
+            for move2 in all_moves - {opposites[move1]}:
+                second = first.move(move2)
+                if second:
+                    if second.head:
+                        disliked_moves.add(move1)
+                    elif second.food and you.health <= 50:
+                        preferred_moves.add(move1)
+
+    # Pick move
+    my_move: Move = "up"
+
+    preferred_moves -= disliked_moves
+    preferred_moves &= possible_moves
+
+    neutral_moves = possible_moves - disliked_moves - preferred_moves
+
+    prio = [
+        ("preferred", preferred_moves),
+        ("neutral", neutral_moves),
+        ("disliked", disliked_moves),
+    ]
+
+    for txt, st in prio:
+        if not st: continue
+        if current_direction in st:
+            print(f"pick current direction from {txt}")
+            my_move = current_direction
+            break
+        else:
+            print(f"pick random from {txt}")
+            my_move = random.choice(list(st))
+            break
 
     return MoveResponse(move=my_move, shout=None)
